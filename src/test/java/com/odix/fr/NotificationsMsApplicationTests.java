766312e23b1a3baa3d08de9093488f9d4fa03ba6
GitLabOdix <azaheni@smartgraphe.com>
2019-12-20 18:46:51 +0100

    first commit

---
+++ src/test/java/com/odix/fr/NotificationsMsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.odix.fr;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NotificationsMsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
