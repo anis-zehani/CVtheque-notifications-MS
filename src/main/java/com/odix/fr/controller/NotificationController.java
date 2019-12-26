85922203f7b54e170444c2d085a19e4ff84a5c7b
GitLabOdix <azaheni@smartgraphe.com>
2019-12-27 00:38:39 +0100

    Mise en place de ce MS

--- src/main/java/com/odix/fr/controller/NotificationController.java
+++ src/main/java/com/odix/fr/controller/NotificationController.java
@@ -8,10 +8,16 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.odix.fr.model.Candidat;
 import com.odix.fr.model.Notification;
+import com.odix.fr.model.Opportunite;
+import com.odix.fr.model.PartenaireTemporaire;
+import com.odix.fr.model.Utilisateur;
 import com.odix.fr.service.NotificationService;
 
 @CrossOrigin
@@ -25,6 +31,30 @@ public class NotificationController {
 	@GetMapping("/all/{idDestinataire}/{etatNotification}")
 	public List<Notification> getAllNotifications(@PathVariable UUID idDestinataire, @PathVariable String etatNotification) {
 	    return notificationService.getAllNotificationsByUtilisateur(idDestinataire, etatNotification);
+	}
+	
+	@PostMapping("/generateSimpleNotification")
+	public void generateSimpleNotification
+	(
+			   @RequestBody String objetNotification, 
+			   @RequestBody String corpsNotification,
+			   @RequestBody List<Utilisateur> listeDestinatairesNotification, 
+			   @RequestBody Utilisateur generateurNotification,
+			   @RequestBody Candidat candidatNotification,
+			   @RequestBody PartenaireTemporaire partenaireNotification,
+			   @RequestBody Opportunite opportuniteNotification
+	) 
+	{
+		notificationService.generateSimpleNotification
+		(
+				objetNotification, 
+				corpsNotification, 
+				listeDestinatairesNotification, 
+				generateurNotification, 
+				candidatNotification, 
+				partenaireNotification, 
+				opportuniteNotification
+		);
 	}
 	
 	@DeleteMapping("/{idNotification}")
